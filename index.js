91bc19f809f4371071ae3a866d9d48e11146938b
@rohit.singh <rohit.singh@firstprinciples.io>
2023-09-26 19:14:30 +0530

    update index.json

--- index.js
+++ index.js
@@ -13,13 +13,15 @@ app.use(cors());
 app.use(express.json({ limit: "500mb"}))
 app.use(express.urlencoded({extended: true}))
 
-
 mongoose.connect(MONGO_URI, {
     useNewUrlParser: true, 
     useUnifiedTopology: true
 }).then( () => console.log(`MOngoDB Connected`)).catch(err => console.log(`DB Connecteion Error`));
 
 
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
 
 // API END POINT
 app.get("/", (req, res) => {
@@ -39,12 +41,4 @@ app.get("/all-tasks", async (req, res) => {
     res.json(response);
 });
 
-
-
-
-
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-})
-
+module.exports = app
