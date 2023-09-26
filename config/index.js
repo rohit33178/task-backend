bc545384898cd9223a72bd1caf80ff12c2dfce21
@rohit.singh <rohit.singh@firstprinciples.io>
2023-09-26 21:16:50 +0530

    update endpoint

--- config/index.js
+++ config/index.js
@@ -1,10 +1,4 @@
-const MONGO_USERNAME = process.env.MONGO_USERNAME
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD
-const MONGO_DB_NAME = process.env.MONGO_DB_NAME
-const MONGO_DB_HOST = process.env.MONGO_DB_HOST
 
-const MONGO_URI = process.env.ENV 
-    ? `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_DB_HOST}/${MONGO_DB_NAME}?retryWrites=true&w=majority` 
-    : "mongodb://127.0.0.1:27017/myTestDB";
+const MONGO_URI = "mongodb://127.0.0.1:27017/myTestDB";
 
 module.exports = MONGO_URI 
