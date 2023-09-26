a94d6e7ed4a6d36891b8977b84eead9bc0a171e7
@rohit.singh <rohit.singh@firstprinciples.io>
2023-09-26 18:50:33 +0530

    update the mongodb connection string

--- config/index.js
+++ config/index.js
@@ -1,5 +1,10 @@
+const MONGO_USERNAME = process.env.MONGO_USERNAME
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD
+const MONGO_DB_NAME = process.env.MONGO_DB_NAME
+const MONGO_DB_HOST = process.env.MONGO_DB_HOST
+
 const MONGO_URI = process.env.ENV 
-    ? `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.on3qis6.mongodb.net/` 
+    ? `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_DB_HOST}/${MONGO_DB_NAME}?retryWrites=true&w=majority` 
     : "mongodb://127.0.0.1:27017/myTestDB";
 
 module.exports = MONGO_URI 
