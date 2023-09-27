534aefa242b19c085c666e00ee60d483a1e4d806
@rohit.singh <rohit.singh@firstprinciples.io>
2023-09-27 14:53:06 +0530

    update dev links

--- index.js
+++ index.js
@@ -13,10 +13,12 @@ app.use(cors());
 app.use(express.json({ limit: "500mb"}))
 app.use(express.urlencoded({extended: true}))
 
+console.log(MONGO_URI);
 mongoose.connect(MONGO_URI, {
     useNewUrlParser: true, 
     useUnifiedTopology: true
-}).then( () => console.log(`MOngoDB Connected`)).catch(err => console.log(`DB Connecteion Error`));
+}).then(() => console.log(`MOngoDB Connected`)).catch(err => console.log(`DB Connecteion Error`));
+
 
 
 
