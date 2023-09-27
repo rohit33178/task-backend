688ef58d18781a3fe35b1526df45ba68d5365c2e
@rohit.singh <rohit.singh@firstprinciples.io>
2023-09-27 14:38:02 +0530

    update db Path

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 
-const MONGO_URI = "mongodb://127.0.0.1:27017/myTestDB";
+const MONGO_URI = process.env.NODE_ENV ? process.env.MONGODB_URI : "mongodb://127.0.0.1:27017/myTestDB";
 
 module.exports = MONGO_URI 
